67dc349c243c5cd6f608b4ba70091fac2323c26c
Dev User <username@users.noreply.github.com>
2025-08-12 17:40:59 -0400

    Removed previous Cursor XY Program

--- XYposition.py
+++
@@ -1,9 +0,0 @@
-# live_xy.py
-import time, pyautogui
-try:
-    while True:
-        x, y = pyautogui.position()
-        print(f"\rX={x:4d} Y={y:4d}", end="", flush=True)
-        time.sleep(0.05)
-except KeyboardInterrupt:
-    print("\nStopped.")
